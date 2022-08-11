ffd3bc3a8b7c461ff1c3876dddc2b66d78c19c7d
Antonio Ricardo Ferreira de Souza <arfs.antonio@gmail.com>
2022-08-11 14:05:47 -0400

    create mutations to REST operations

--- api/user/resolvers/userResolvers.js
+++ api/user/resolvers/userResolvers.js
@@ -5,6 +5,11 @@ const userResolvers = {
   Query: {
     users: (root, args, { dataSources }) => dataSources.usersApi.getUsers(),
     user: (root, { id }, { dataSources }) => dataSources.usersApi.getUserById(id),
+  },
+  Mutation: {
+    addUser: (root, user, { dataSources }) => dataSources.usersApi.addUser(user),
+    updateUser: (root, newData, { dataSources }) => dataSources.usersApi.updateUser(newData),
+    deleteUser: (root, { id }, { dataSources }) => dataSources.usersApi.deleteUser(id),
   }
 };
 
