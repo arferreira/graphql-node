4bf3db528d2fbe2c6a46f42e6365dd7f1e6ead4c
Antonio Ricardo Ferreira de Souza <arfs.antonio@gmail.com>
2022-08-11 14:34:14 -0400

    work with scalar custom

--- api/user/resolvers/userResolvers.js
+++ api/user/resolvers/userResolvers.js
@@ -1,7 +1,14 @@
-
+const { GraphQLScalarType } = require("graphql");
 
 
 const userResolvers = {
+  DateTime: new GraphQLScalarType({
+    name: "DateTime",
+    description: "string to datetime on format ISO-8601",
+    serialize: (value) => value.toISOString(),
+    parseValue: (value) => new Date(value),
+    parseLiteral: (ast) => new Date(ast.value),
+  }),
   Query: {
     users: (root, args, { dataSources }) => dataSources.usersApi.getUsers(),
     user: (root, { id }, { dataSources }) => dataSources.usersApi.getUserById(id),
